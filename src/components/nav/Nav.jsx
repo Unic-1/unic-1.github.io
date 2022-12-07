4e9ddb839d9e698faa566dc87bb3e90d8176ff87
satyam <satyam123.sr@gmail.com>
2022-12-07 14:23:32 +0530

    - active nav tab style logic added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,13 +7,18 @@ import {CgWebsite} from 'react-icons/cg';
 import {MdOutlineContactPhone} from 'react-icons/md';
 
 const Nav = () => {
+  const [activeTab, setActiveTab] = React.useState('#hero')
+
+  function isActive(tab) {
+    return activeTab === tab ? 'active' : ''
+  }
   return (
     <nav>
-      <a href="#hero"><BiHomeAlt /></a>
-      <a href="#about"><FaRegUser/></a>
-      <a href="#experience"><SlEnergy/></a>
-      <a href="#project"><CgWebsite/></a>
-      <a href="#contact"><MdOutlineContactPhone/></a>
+      <a href="#hero" className={isActive('#hero')} onClick={() => setActiveTab('#hero')}><BiHomeAlt /></a>
+      <a href="#about" className={isActive('#about')} onClick={() => setActiveTab('#about')}><FaRegUser/></a>
+      <a href="#experience" className={isActive('#experience')} onClick={() => setActiveTab('#experience')}><SlEnergy/></a>
+      <a href="#project" className={isActive('#project')} onClick={() => setActiveTab('#project')}><CgWebsite/></a>
+      <a href="#contact" className={isActive('#contact')} onClick={() => setActiveTab('#contact')}><MdOutlineContactPhone/></a>
     </nav>
   )
 }
