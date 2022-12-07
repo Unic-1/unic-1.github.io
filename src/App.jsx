07167bb3e71dda0450e430502948f90fad982466
satyam <satyam123.sr@gmail.com>
2022-12-07 14:51:12 +0530

    - Nav added to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import './style.css'
 
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
 
 
 const App = () => {
   return (
     <>
       <Header />
+      <Nav />
     </>
   )
 }
