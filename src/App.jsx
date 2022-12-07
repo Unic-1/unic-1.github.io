cab39ae6ef7eee3b6fbd79445dfd31882a3287eb
satyam <satyam123.sr@gmail.com>
2022-12-07 17:28:00 +0530

    - experience section added to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './style.css'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
+import Experience from './components/experience/Experience'
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <Nav />
       <About />
+      <Experience />
     </>
   )
 }
